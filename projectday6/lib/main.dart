51600b3430f6ff156f145586be828ed4570ac802
shishupaltech <veerutech75@gmail.com>
2025-06-18 11:22:36 +0530

    Day 6:Floatingaction button is done

--- projectday6/lib/main.dart
+++ projectday6/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,52 +13,51 @@ class MyApp extends StatelessWidget {
       title: 'Project Day 6',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red)
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
       ),
       home: Scaffold(
         appBar: AppBar(
           leading: Icon(Icons.person),
           title: Text('Day 6'),
           actions: [
-
             Icon(Icons.login),
-            SizedBox(width: 20,),
+            SizedBox(width: 20),
             Icon(Icons.logout),
-            SizedBox(width: 20,),
+            SizedBox(width: 20),
           ],
           centerTitle: true,
           backgroundColor: Colors.grey,
           foregroundColor: Colors.white,
-          
         ),
-        body: Column(
-          children: [],
+        body: Column(children: []),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.amber,
+          foregroundColor: Colors.purple,
+          onPressed: () {
+            print('hello');
+          },
+          child: Icon(Icons.add),
         ),
-
         bottomNavigationBar: BottomNavigationBar(
-            items:[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-                tooltip: 'Go to Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Search',
-                tooltip: 'Go to Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Profile',
-                tooltip: 'Go to Profile',
-              )
-
-
-
-            ]
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              tooltip: 'Go to Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              tooltip: 'Go to Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+              tooltip: 'Go to Profile',
+            ),
+          ],
         ),
       ),
-
     );
   }
 }
