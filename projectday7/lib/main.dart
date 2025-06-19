6749298fd81cb89aa98cd990c96bbdc2e57a9b61
shishupaltech <veerutech75@gmail.com>
2025-06-19 12:08:24 +0530

    day 7: completed

--- projectday7/lib/main.dart
+++ projectday7/lib/main.dart
@@ -1,46 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:projectday7/pages/home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-
-  int age = 20;
-  final double price = 2323.23;
-  final String name = "Shishupal Singh";
-  final bool value = true;
-  var person = 25;
-  dynamic myvalue = 'karan';
-  List<String> list= ["Apple","Banana","Mango","karan","soniya"];
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    HomePage(),
+    // Center(child: Text('Home', style: TextStyle(fontSize: 35))),
+    Center(child: Text('Search', style: TextStyle(fontSize: 35))),
+    Center(child: Text('Profile', style: TextStyle(fontSize: 35))),
+  ];
+  @override
   Widget build(BuildContext context) {
-    myvalue  = 23;
     return MaterialApp(
-      title: 'Project Day 7',
+      title: 'Project Day 6',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Data types in UI'),
+          // leading: Icon(Icons.person),
+          title: Text('Day 6'),
+          actions: [
+            Icon(Icons.login),
+            SizedBox(width: 20),
+            Icon(Icons.logout),
+            SizedBox(width: 20),
+          ],
+          centerTitle: true,
+          backgroundColor: Colors.grey,
+          foregroundColor: Colors.white,
         ),
-        body: SingleChildScrollView(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        body: _pages[_currentIndex],
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.amber,
+          foregroundColor: Colors.purple,
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        drawer: Drawer(
+          child: ListView(
             children: [
-              Text('$age'),
-              Text('$price'),
-              Text('$name'),
-              Text("Is Student? bool ${value? "yes" : "No" }"),
-              Text("$person"),
-              Text("$myvalue"),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:list.map((fruit)=>Text("$fruit")).toList(),
-              )
-
+              DrawerHeader(child: Text('Hello')),
+              ListTile(leading: Icon(Icons.home), title: Text('Home')),
+              ListTile(leading: Icon(Icons.home), title: Text('Home')),
             ],
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          onTap: (value) => {
+            setState(() {
+              _currentIndex = value;
+            }),
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              tooltip: 'Go to Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              tooltip: 'Go to Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+              tooltip: 'Go to Profile',
+            ),
+          ],
         ),
       ),
     );
