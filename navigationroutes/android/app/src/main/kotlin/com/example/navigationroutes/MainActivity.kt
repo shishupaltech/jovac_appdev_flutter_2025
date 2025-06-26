b3b071a976a1a70f4c62531568be2ac2371080a4
shishupaltech <veerutech75@gmail.com>
2025-06-26 11:26:37 +0530

    Navigation and routes done with named and without name

---
+++ navigationroutes/android/app/src/main/kotlin/com/example/navigationroutes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.navigationroutes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ navigationroutes/lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigator or Pages',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/about': (context) => AboutPage(),
+        '/contact':(context)=>ContactPage(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Screen')),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Push worked with MaterialPageRoute'),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => AboutPage()),
+            ),
+          ),
+          ListTile(
+            title:Text('PushNamed worked with route!..'),
+            onTap: ()=> Navigator.pushNamed(
+              context,
+              '/about'
+            ),
+          ),
+          ListTile(
+            title:Text('PushReplacement worked with materialpageroute!..'),
+            onTap: ()=> Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (_) => ContactPage()),
+            ),
+          ),
+          ListTile(
+            title:Text('PushReplacementNamed worked with route!..'),
+            onTap: ()=> Navigator.pushReplacementNamed(
+              context,
+              '/contact')
+          ),
+          ListTile(
+            title: Text('PushAndRemoveUntil worked with materialpageroute!..'),
+            onTap: () => Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (_) => AboutPage()),
+              (route) => false,
+            ),
+          ),
+          ListTile(
+            title: Text('PushNamedAndRemoveUntil worked with route!..'),
+            onTap: () => Navigator.pushNamedAndRemoveUntil(
+              context,
+              '/about',
+              (route) => false,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('About Page'), centerTitle: true),
+      body: Center(child: Column(children: [Text('Hello!..')])),
+    );
+  }
+}
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact Page'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Contact'),
+            ElevatedButton(onPressed: ()=>Navigator.pop(context), child: Text('Back'))
+          ],
+
+        )
+      ),
+    );
+  }
+}
